c9d3251b5666447efece6a4e1a09b66dc07308d2
Leo Buck <leonardo@timais.com>
2021-01-04 13:08:59 -0300

    first commit

---
+++ src/test/java/com/leo/cursoapirest/CursoApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.cursoapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
