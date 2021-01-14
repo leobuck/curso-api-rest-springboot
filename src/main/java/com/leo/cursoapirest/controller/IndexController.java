4de69d6a49cca45f7c9715ed029308efbed19d52
Leo Buck <leonardo@timais.com>
2021-01-13 21:25:59 -0300

    Retornando usuários em JSON

--- src/main/java/com/leo/cursoapirest/controller/IndexController.java
+++ src/main/java/com/leo/cursoapirest/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.leo.cursoapirest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +10,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.leo.cursoapirest.model.Usuario;
+
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
 
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity index(@RequestParam(value = "nome", defaultValue = "Nome não informado", required = true) String nome,
-			@RequestParam("salario") Long salario) {
-		System.out.println("Parâmetro nome: " + nome);
-		System.out.println("Parâmetro salario: " + salario);
-		return new ResponseEntity("Olá " + nome + ", salário R$ " + salario, HttpStatus.OK);
+	public ResponseEntity<List<Usuario>> index() {
+		Usuario usuario = new Usuario();
+		usuario.setId(1L);
+		usuario.setLogin("leo");
+		usuario.setNome("Leo");
+		usuario.setSenha("123");
+		
+		Usuario usuario2 = new Usuario();
+		usuario2.setId(2L);
+		usuario2.setLogin("ana");
+		usuario2.setNome("Ana");
+		usuario2.setSenha("123");
+		
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		usuarios.add(usuario);
+		usuarios.add(usuario2);
+		
+		return ResponseEntity.ok(usuarios);
 	}
 }
