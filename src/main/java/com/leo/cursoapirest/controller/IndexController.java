92560e8e87fe53dc836cefa213219a7e208129be
Leo Buck <leonardo@timais.com>
2021-01-13 21:17:12 -0300

    Passando parametros para o controller

--- src/main/java/com/leo/cursoapirest/controller/IndexController.java
+++ src/main/java/com/leo/cursoapirest/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity index() {
-		return new ResponseEntity("Olá usuário!", HttpStatus.OK);
+	public ResponseEntity index(@RequestParam(value = "nome", defaultValue = "Nome não informado", required = true) String nome,
+			@RequestParam("salario") Long salario) {
+		System.out.println("Parâmetro nome: " + nome);
+		System.out.println("Parâmetro salario: " + salario);
+		return new ResponseEntity("Olá " + nome + ", salário R$ " + salario, HttpStatus.OK);
 	}
 }
