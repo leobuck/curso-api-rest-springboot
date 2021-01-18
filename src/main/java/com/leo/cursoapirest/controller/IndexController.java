2c70c35b6f32b36139671b5e723a33293fdb6501
leobuck <leobuck12@gmail.com>
2021-01-18 13:08:13 -0300

    Criando PUT para atualização de usuário

--- src/main/java/com/leo/cursoapirest/controller/IndexController.java
+++ src/main/java/com/leo/cursoapirest/controller/IndexController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +46,13 @@ public class IndexController {
 	
 	@PostMapping(value = "/", produces = "application/json")
 	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return ResponseEntity.ok(usuarioSalvo);
+	}
+	
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
 		Usuario usuarioSalvo = usuarioRepository.save(usuario);
 		
 		return ResponseEntity.ok(usuarioSalvo);
