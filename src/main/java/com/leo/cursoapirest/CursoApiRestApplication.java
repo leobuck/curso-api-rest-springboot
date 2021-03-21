977571b3a0580d06a96f6aaddaeb47dd23b03361
leobuck <leobuck12@gmail.com>
2021-03-21 15:45:26 -0300

    Ajuste ApplicationContext

--- src/main/java/com/leo/cursoapirest/CursoApiRestApplication.java
+++ src/main/java/com/leo/cursoapirest/CursoApiRestApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,7 +25,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CursoApiRestApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CursoApiRestApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(CursoApiRestApplication.class, args);
+		ApplicationContextLoad applicationContextLoad = new ApplicationContextLoad();
+		applicationContextLoad.setApplicationContext(applicationContext);
 		System.out.println(new BCryptPasswordEncoder().encode("123"));
 	}
 
