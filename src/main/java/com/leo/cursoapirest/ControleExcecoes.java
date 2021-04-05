5dc8a1a03b88fd7c7f1eec96ee27aadb0259db69
leobuck <leobuck12@gmail.com>
2021-04-04 21:38:19 -0300

    Controle de erros da API - Parte 2

--- src/main/java/com/leo/cursoapirest/ControleExcecoes.java
+++ src/main/java/com/leo/cursoapirest/ControleExcecoes.java
@@ -1,8 +1,12 @@
 package com.leo.cursoapirest;
 
+import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.List;
 
+import org.hibernate.exception.ConstraintViolationException;
+import org.postgresql.util.PSQLException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -39,5 +43,30 @@ public class ControleExcecoes extends ResponseEntityExceptionHandler {
 		objetoErro.setCode(status.value() + " -> " + status.getReasonPhrase());
 		
 		return new ResponseEntity<>(objetoErro, headers, status);
+	}
+	
+	@ExceptionHandler({DataIntegrityViolationException.class, ConstraintViolationException.class,
+		PSQLException.class, SQLException.class})
+	protected ResponseEntity<Object> handleExceptionDataIntegrity(Exception ex) {
+		
+		String msg = "";
+		
+		if (ex instanceof DataIntegrityViolationException) {
+			msg = ((DataIntegrityViolationException) ex).getCause().getCause().getMessage();
+		} else if (ex instanceof ConstraintViolationException) {
+			msg = ((ConstraintViolationException) ex).getCause().getCause().getMessage();
+		} else if (ex instanceof PSQLException) {
+			msg = ((PSQLException) ex).getCause().getCause().getMessage();
+		}  else if (ex instanceof SQLException) {
+			msg = ((SQLException) ex).getCause().getCause().getMessage();
+		} else {
+			msg = ex.getMessage();
+		}
+		
+		ObjetoErro objectError = new ObjetoErro();
+		objectError.setCode(msg);
+		objectError.setCode(HttpStatus.INTERNAL_SERVER_ERROR + " -> " + HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+		
+		return new ResponseEntity<>(objectError, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
