4352a8aa30b725a9ae9ff3449e36e4c36b152c6a
leobuck <leobuck12@gmail.com>
2021-05-09 19:53:39 -0300

    Recuperação de login por email 2

---
+++ src/main/java/com/leo/cursoapirest/service/EmailService.java
@@ -0,0 +1,47 @@
+package com.leo.cursoapirest.service;
+
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailService {
+
+	private String userName = "sistemaolimpo2018@gmail.com";
+	private String password = "franpaodequeijo";
+
+	public void enviarEmail(String assunto, String emailDestino, String mensagem) throws MessagingException {
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls", "true");
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.port", "465");
+		properties.put("mail.smtp.socketFactory.port", "465");
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		
+		Session session = Session.getInstance(properties, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(userName, password);
+			}
+		});
+		
+		Address[] toUser = InternetAddress.parse(emailDestino);
+		
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(userName));
+		message.setRecipients(Message.RecipientType.TO, toUser);
+		message.setSubject(assunto);
+		message.setText(mensagem);
+		
+		Transport.send(message);
+	}
+}
