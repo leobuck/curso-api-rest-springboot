88eb83e25a2246f6776f9b5a44d71d753e02c7b3
leobuck <leobuck12@gmail.com>
2021-05-09 22:12:41 -0300

    Serviço de Geração de Relatório

---
+++ src/main/java/com/leo/cursoapirest/service/RelatorioService.java
@@ -0,0 +1,37 @@
+package com.leo.cursoapirest.service;
+
+import java.io.File;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+@Service
+public class RelatorioService implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public byte[] gerarRelatorio(String nomeRelatorio, ServletContext servletContext) throws SQLException, JRException {
+		Connection connection = jdbcTemplate.getDataSource().getConnection();
+		
+		String caminhoJasper = servletContext.getRealPath("relatorios") + File.separator + nomeRelatorio + ".jasper";
+		
+		JasperPrint print = JasperFillManager.fillReport(caminhoJasper, new HashMap<>(), connection);
+		
+		return JasperExportManager.exportReportToPdf(print);
+	}
+}
