787210615cb81094a08875cb1fe3939e8d0ad81c
leobuck <leobuck12@gmail.com>
2021-06-06 18:13:26 -0300

    Validação de Token Expirado.

--- src/main/java/com/leo/cursoapirest/security/JWTTokenAutenticacaoService.java
+++ src/main/java/com/leo/cursoapirest/security/JWTTokenAutenticacaoService.java
@@ -89,9 +89,8 @@ public class JWTTokenAutenticacaoService {
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
+			liberacaoCors(response);
 		}
-		
-		//liberacaoCors(response);
 		
 		return null;
 	}
